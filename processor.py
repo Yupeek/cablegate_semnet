fd16d04ed264784046b04cc207e44d7706eadc39
Mark A. Matienzo <mark@matienzo.org>
2010-11-30 22:47:40 -0500

    processor.Cable now creates python objects, according to ACP-127 parsing rules
    took typecode's existing code and improved it to make it more general -
    this way we can reuse parsing code regardless of where data will end
    up (mongodb, solr, etc.)

--- processor.py
+++ processor.py
@@ -6,61 +6,73 @@ import xml
 import csv
 import subprocess
 
-import nltk
-
-sys.path.append("lib")
-import pymongo
 from BeautifulSoup import BeautifulSoup
+
+LF = '&#x000A;'
+PRECEDENCE = {'ZZ': 'FLASH',
+              'OO': 'IMMEDIATE',
+              'PP': 'PRIORITY',
+              'RR': 'ROUTINE'}
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
 
-mongo_conn = pymongo.Connection('localhost', 27017)
-db = mongo_conn['wikileaks']
+def get_soup_text(bs):
+  return ''.join(bs.findAll(text=True)).strip()
 
-class Cable():
+class Cable(object):
   
   raw = ""
-  attrs = {}
+  id = None
   
-  def __init__(self,raw):
+  def __init__(self, raw, extract_content=True):
+    #self.raw = raw
     logging.info('Cable()')
-    self.raw = raw
+    if extract_content is True:
+      self.extract_content(raw)
   
-  def __getitem__(self,name):
-    if name == 'raw':
-      return self.raw
-    if name in self.attrs:
-      return self.attrs[name]
-    else:
-      return None
-    
-  def __setitem__(self,name,value):
-    self.attrs[name] = value
-    
-  def get(self):
-    return self.attrs
-    
-class CableGateMirror():
-  
-  mirror_directory = 'data/cablegate/'
-  
-  def __init__(self):
-    logging.info('CableGateMirror()')
-    self.update()
-    
-  def update(self):
-    logging.info('CableGateMirror.update')
-    #subprocess.call(["httrack",'--update'],cwd=self.mirror_directory)
-    Processor()
-    
+  def parse_wikileaks_header(self, bs):
+    top = bs.find("table", { "class" : "cable" }).findAll('td')
+    self.id = get_soup_text(top[0])
+    self.datetime = get_soup_text(top[1])
+    self.released = get_soup_text(top[2])
+    self.classification = get_soup_text(top[3])
+    self.origin = get_soup_text(top[4])
 
+  def parse_cable_header(self, bs):
+    header = bs.findAll('pre')[0]
+    self.raw_header_display = get_soup_text(header)
+    hdlst = self.raw_header_display.split(LF, 6)
+    self.tx_id = hdlst[0]
+    self.precedence = PRECEDENCE[hdlst[1][0:2]]
+    self.destination_ri = hdlst[1][3:].split(' ')
+    foo, self.origin_ri, self.origin_serno, self.tx_time = hdlst[2].split(' ')
+    fl4 = hdlst[3].split(' ')
+    self.opsig = [fl4[0]]
+    self.classgrp = fl4[1]
+    if len(fl4) > 2:
+      self.opsig.extend(fl4[2:])
+    self.datetimegrp = hdlst[4]
+    self.origin_plad = hdlst[5][3:]
+    # just a hack for now...
+    self.destination_plad = hdlst[6]
+
+  def parse_cable_body(self, bs):
+    self.body = get_soup_text(bs.findAll('pre')[1])
+
+  def extract_content(self, raw):
+    logging.info('Cable.extract_content')
+    soup = BeautifulSoup(raw)
+    self.parse_wikileaks_header(soup)
+    self.parse_cable_header(soup)
+    self.parse_cable_body(soup)
+    
 class Processor():
   
   data_directory = 'data/cablegate/cablegate.wikileaks.org/cable'
   
   country_dictionary_path = 'data/countries.csv'
   countries = []
-  country_frequency = nltk.probability.FreqDist()
+  #country_frequency = nltk.probability.FreqDist()
   
   file_regex = re.compile("\.html$")
   
@@ -73,11 +85,11 @@ class Processor():
   def __init__(self):
     logging.info('Processor()')
     self.load_countries()
-    self.process()
+#    self.process()
   
-  def process(self):
-    logging.info('Processor.process')
-    self.read_files()
+  # def process(self):
+  #   logging.info('Processor.process')
+  #   self.read_files()
   
   def load_countries(self):
     logging.info('Processor.load_countries')
@@ -111,41 +123,6 @@ class Processor():
       self.counts['files_not_processed'] = self.counts['files_not_processed'] + 1
       return
     self.extract_content(file.read())
-    
-  def extract_content(self,raw):
-    logging.info('Processor.extract_content')
-    
-    soup = BeautifulSoup(raw)
-    cable_table = soup.find("table", { "class" : "cable" })
-    cable_id = cable_table.findAll('tr')[1].findAll('td')[0]\
-      .contents[1].contents[0]
-    if db.cables.find_one({'_id':cable_id}):
-      self.counts['files_not_processed'] = self.counts['files_not_processed'] + 1
-      logging.info('Processor.extract_content["CABLE ALREADY EXISTS"]')
-      self.print_counts()
-      return
-      
-    cable = Cable(raw)
-    cable['_id'] = cable_id
-    cable['reference_id'] = cable_id
-    cable['date_time'] = cable_table.findAll('tr')[1].findAll('td')[1]\
-      .contents[1].contents[0]
-    cable['classification'] = cable_table.findAll('tr')[1].findAll('td')[2]\
-      .contents[1].contents[0]
-    cable['origin'] = cable_table.findAll('tr')[1].findAll('td')[3]\
-      .contents[1].contents[0]
-    cable['header'] = nltk.clean_html(str(soup.findAll(['pre'])[0]))
-    cable['body'] = nltk.clean_html(str(soup.findAll(['pre'])[1]))
-    
-    db.cables.insert(cable.get())
-    
-    self.counts['files_processed'] = self.counts['files_processed'] + 1
-    
-    self.print_counts()
-    
-    if (self.counts['files_processed'] + self.counts['files_not_processed'])\
-      == self.counts['files_to_process']:
-      self.dump_json()
   
   def print_counts(self):
     logging.info('Processor.print_counts')
@@ -155,6 +132,4 @@ class Processor():
   
   def dump_json(self):
     logging.info('Processor.dump_json')
-    
-CableGateMirror()
-    
+ 
